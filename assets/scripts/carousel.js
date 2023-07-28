f29b2dd0692548685c2b56097723e678a126c42d
Pedro Martins <pedroporcino2@gmail.com>
2023-07-28 10:58:18 -0300

    Change the script of the carousel

--- assets/scripts/carousel.js
+++ assets/scripts/carousel.js
@@ -1,12 +1,14 @@
+const btnRight = document.querySelector('.arrow-btn.-right');
+const btnLeft = document.querySelector('.arrow-btn.-left'); 
+const carousel = document.querySelector('.emmetCard');
+let pixels = 0;
 
-function arrowMove(x) {
+btnRight.addEventListener('click', function() {
+    pixels = pixels - 100;
+    carousel.style = `transform: translateX(${pixels}px)`
+});
 
-    var move = document.querySelector(".emmetCard")
-
-    if (x == 1) {
-        move.scrollLeft = move.scrollLeft - 200;
-    } else if (x == 2) {
-        move.scrollLeft = move.scrollLeft + 100;
-    }
-
-}
+btnLeft.addEventListener('click', function() {
+    pixels = pixels + 100;
+    carousel.style = `transform: translateX(${pixels}px)`
+})
